74fce45679cd0cc7787c541a93ecd0450689f715
Dane Hale <danehale0612@gmail.com>
2013-06-04 11:58:29 -0500

    Extremely happy path of article create

--- test/integration/anonymous_broswes_article_test.rb
+++ test/integration/anonymous_broswes_article_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class AnonymousUserPostAricleTest < ActionDispatch::IntegrationTest
   test "view list of aricles" do
     Post.create!(title: "First Post!!", body: "This is awkward... I have nothing to say :(")
-    Post.create!(title: "Wow, it's been a while!", body: "And yet, I'm still not a writer")
+    Post.create!(title: "Wow, it has been a while!", body: "And yet, I'm still not a writer")
 
     visit '/posts'
     assert_equal 2, Post.count
     assert_include page.body, "All Posts"
     assert_include page.body, "First Post!!"
-    assert_include page.body, "Wow, it's been a while!"
+    assert_include page.body, "Wow, it has been a while!"
   end
 end
--- test/integration/anonymous_post_aricle_test.rb
+++ test/integration/anonymous_post_aricle_test.rb
@@ -6,8 +6,9 @@ class AnonymousPostsAricleTest < ActionDispatch::IntegrationTest
     click_link 'Write Post'
     fill_in 'Title', with: 'My First Post!!'
     fill_in 'Body', with: "I don't have anything to say"
+    click_button 'Submit Post'
     assert_include page.body, "Your post has now been published!"
     visit '/posts'
-    assert_include page.body 'My First Post!!'
+    assert_include page.body, 'My First Post!!'
   end
 end

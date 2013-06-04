7aa204efd9841a9369bddc50b8872ad5579dc5b2
Dane Hale <danehale0612@gmail.com>
2013-06-04 10:33:36 -0500

    User Browse list of posts

--- test/integration/anonymous_user_post_aricle_test.rb
+++ test/integration/anonymous_user_post_aricle_test.rb
@@ -5,12 +5,10 @@ class AnonymousUserPostAricleTest < ActionDispatch::IntegrationTest
     Post.create!(title: "First Post!!", body: "This is awkward... I have nothing to say :(")
     Post.create!(title: "Wow, it's been a while!", body: "And yet, I'm still not a writer")
 
-    visit '/'
-    click_link "View All Posts"
-    assert_current_path '/posts'
-    #assume we have a page variable
-    assert_include page.content, "First Post!!"
-    #=> assert page.content.include? "First Post!!"
-    assert_include page.content, "Wow, it's been a while!"
+    visit '/posts'
+    assert_equal 2, Post.count
+    assert_include page.body, "All Posts"
+    assert_include page.body, "First Post!!"
+    assert_include page.body, "Wow, it's been a while!"
   end
 end
